c3bc23c2a19809a16061e2a2b5406fb11c5005ec
Alexey Novak <alexey.novak.mail@gmail.com>
2015-03-30 17:41:28 -0400

    ui-01: refactored all components to attach methods separately as per https://github.com/airbnb/javascript/blob/master/README.md#constructors

--- server/public/js/components/map/mapView.js
+++ server/public/js/components/map/mapView.js
@@ -3,6 +3,7 @@
 var tgv = tgv || {};
 
 (function(appModel, utils) {
+
   var MapView = function(options) {
     this._init = this._init.bind(this);
     this.addMapControl = this.addMapControl.bind(this);
@@ -22,95 +23,106 @@ var tgv = tgv || {};
   };
 
   MapView.prototype = {
-    _init: function MapView__init(options) {
-      this._control = options.control;
-
-      var mapCanvas = document.querySelector(options.componentSelector);
-
-      var mapOptions = {
-        center: new google.maps.LatLng(21.2125, 31.1973),
-        zoom: 3,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        streetViewControl: false
-      };
-
-      this._googleMap = new google.maps.Map(mapCanvas, mapOptions);
-
-      // bind events
-      google.maps.event.addListener(this._googleMap, 'click', function(event) {
-        var lat = event.latLng.lat(),
-            lng = event.latLng.lng();
-
-        this._control.mapClick(event.pixel.x, event.pixel.y, lat, lng);
-      }.bind(this));
-    },
-
-    getCircleBounds: function MapView_getCircleBounds() {
-      if (!this._circle) {
-        return;
-      }
-
-      return this._circle.getBounds();
-    },
-
-    addMapCircle: function MapView_addMapCircle(lat, lng) {
-      this.removeCircle();
-
-      this._circle = new google.maps.Circle({
-        map: this._googleMap,
-        center: new google.maps.LatLng(lat, lng),
-        clickable: false,
-        radius: appModel.getClickRadiusMeters(),
-        fillColor: '#fff',
-        fillOpacity: 0.6,
-        strokeColor: '#313131',
-        strokeOpacity: 0.4,
-        strokeWeight: 0.8
-      });
-
-      this._googleMap.setOptions({
-        draggable: false,
-        zoomControl: false,
-        scrollwheel: false,
-        panControl: false,
-        disableDoubleClickZoom: true
-      });
-    },
-
-    removeCircle: function MapView_removeCircle() {
-      if (!this._circle) {
-        return;
-      }
-
-      this._circle.setMap(null);
-      this._googleMap.setOptions({
-        draggable: true,
-        zoomControl: true,
-        scrollwheel: true,
-        panControl: true,
-        disableDoubleClickZoom: false
-      });
-    },
-
-    addMapControl: function MapView_addMapControl(location, el) {
-      this._googleMap.controls[location].push(el);
-    },
-
-    renderHeatMap: function MapView_renderHeatMap(heatmapData) {
-      //insert heatmap layer
-      var heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatmapData,
-        dissipating: false,
-        radius: 5,
-        map: this._googleMap
-      });
-    },
-
     el: null,
     _circle: null,
     _control: null,
     _googleMap: null
   };
 
+  MapView.prototype._init = function MapView__init(options) {
+    this._control = options.control;
+
+    this.el = document.querySelector(options.componentSelector);
+
+    var mapOptions = {
+      center: new google.maps.LatLng(21.2125, 31.1973),
+      zoom: 3,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      streetViewControl: false
+    };
+
+    this._googleMap = new google.maps.Map(this.el, mapOptions);
+
+    // bind events
+    google.maps.event.addListener(this._googleMap, 'click', function(event) {
+      var lat = event.latLng.lat(),
+          lng = event.latLng.lng();
+
+      this._control.mapClick(event.pixel.x, event.pixel.y, lat, lng);
+    }.bind(this));
+  };
+
+  MapView.prototype.getCircleBounds = function MapView_getCircleBounds() {
+    if (!this._circle) {
+      return;
+    }
+
+    return this._circle.getBounds();
+  };
+
+  MapView.prototype.addMapCircle = function MapView_addMapCircle(lat, lng) {
+    this.removeCircle();
+
+    this._circle = new google.maps.Circle({
+      map: this._googleMap,
+      center: new google.maps.LatLng(lat, lng),
+      clickable: false,
+      radius: appModel.getClickRadiusMeters(),
+      fillColor: '#fff',
+      fillOpacity: 0.6,
+      strokeColor: '#313131',
+      strokeOpacity: 0.4,
+      strokeWeight: 0.8
+    });
+
+    this._googleMap.setOptions({
+      draggable: false,
+      zoomControl: false,
+      scrollwheel: false,
+      panControl: false,
+      disableDoubleClickZoom: true
+    });
+
+    return this; // for chaining
+  };
+
+  MapView.prototype.removeCircle = function MapView_removeCircle() {
+    if (!this._circle) {
+      return;
+    }
+
+    this._circle.setMap(null);
+    this._googleMap.setOptions({
+      draggable: true,
+      zoomControl: true,
+      scrollwheel: true,
+      panControl: true,
+      disableDoubleClickZoom: false
+    });
+
+    return this; // for chaining
+  };
+
+  MapView.prototype.addMapControl =
+                                  function MapView_addMapControl(location, el) {
+    this._googleMap.controls[location].push(el);
+
+    return this; // for chaining
+  };
+
+  MapView.prototype.renderHeatMap =
+                                  function MapView_renderHeatMap(heatmapData) {
+    //insert heatmap layer
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: heatmapData,
+      dissipating: false,
+      radius: 5,
+      map: this._googleMap
+    });
+
+    return this; // for chaining
+  };
+
   tgv.MapView = MapView;
+
 })(tgv.appModel, tgv.utils);
--- server/public/js/components/tweetsPopup/tweetsPopup.js
+++ server/public/js/components/tweetsPopup/tweetsPopup.js
@@ -1,7 +1,8 @@
 var tgv = tgv || {};
 
 (function(appModel, utils, events, TweetsPopupView) {
-  var TweetsPopup= function(options) {
+
+  var TweetsPopup = function(options) {
     this._init = this._init.bind(this);
     this._showPopup = this._showPopup.bind(this);
     this._hidePopup = this._hidePopup.bind(this);
@@ -17,34 +18,36 @@ var tgv = tgv || {};
   };
 
   TweetsPopup.prototype = {
-    _init: function TweetsPopup__init(options) {
-      this.view = new TweetsPopupView(options);
-
-      // bind events
-      events.on('mapClick', this._showPopup);
-    },
-
-    _showPopup: function TweetsPopup__showPopup(x, y, tweets) {
-      tweets = tweets || [];
-
-      this.showing = true;
-      appModel.showingTweetsPopup = this.showing;
-
-      this.view.show(x, y, tweets);
-    },
-
-    _hidePopup: function TweetsPopup__hidePopup() {
-      this.showing = false;
-      appModel.showingTweetsPopup = this.showing;
-
-      this.view.hide();
-
-      events.emit('closePopup');
-    },
-
     showing: false,
     view: null
   };
 
+  TweetsPopup.prototype._init = function TweetsPopup__init(options) {
+    this.view = new TweetsPopupView(options);
+
+    // bind events
+    events.on('mapClick', this._showPopup);
+  };
+
+  TweetsPopup.prototype._showPopup =
+                                function TweetsPopup__showPopup(x, y, tweets) {
+    tweets = tweets || [];
+
+    this.showing = true;
+    appModel.showingTweetsPopup = this.showing;
+
+    this.view.show(x, y, tweets);
+  };
+
+  TweetsPopup.prototype._hidePopup = function TweetsPopup__hidePopup() {
+    this.showing = false;
+    appModel.showingTweetsPopup = this.showing;
+
+    this.view.hide();
+
+    events.emit('closePopup');
+  };
+
   tgv.TweetsPopup = TweetsPopup;
+
 })(tgv.appModel, tgv.utils, tgv.events, tgv.TweetsPopupView);
