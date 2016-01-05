5e9b1c5cfff01d2a9e4c55b89c9f58ba6b777a82
Carlos Carcamo <carloscarcamo.m@gmail.com>
2016-01-05 16:38:30 -0600

    Changes:
     - Bug #20 fixed
     - Allow to search multiple keywords (#18)

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -4,43 +4,54 @@
 
 var monk = require('monk'),
     uuid = require('node-uuid'),
-    mongoConfig = require('../config/config.json').mongo;
+    mongoConfig = require('../config/config.json').mongo,
+    Promise = require('promise'),
+    validator = require('validator');
 
 var tweets = {
   get: function(req, res, next) {
     var db = monk(mongoConfig.server + mongoConfig.databaseName),
-        collection = db.get(mongoConfig.collection);
+        collection = db.get(mongoConfig.collection),
+        q = req.query.search.split(','),
+        queries = [],
+        features = [];
 
-    //execute db query
-    collection.find({
-      $text: {
-        $search: req.query.search
-      }
-    }, { tln: 1, tlt: 1 }, function(error, cursor) {
+    // multiple find statements for search multiple keywords
+    q.forEach(function(value) {
+      var val = validator.trim(validator.escape(value));
+      var query = {
+        $text: {
+          $search: '"'+val+'"' // double quotes helps to search phrases with a space
+        }
+      };
+      var promise = collection.find(query, { tln: 1, tlt: 1 });
+      queries.push(promise);
+    });
 
-      var features = [];
+    // getting all results
+    Promise.all(queries).then(function(values) {
 
-      if (error) {
-        return next(error);
-      }
-
-      cursor = cursor || [];
+      values = values || [];
 
       //chew up each database entry into geoJSON;
       //render the page with the data overlay once we reach the end of the list of matches.
-      cursor.forEach(function(item) {
-        if (!item) {
-          return;
-        }
+      values.forEach(function(cursor) {
+        cursor = cursor || [];
 
-        features.push({
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [item.tln, item.tlt]
-          },
-          text: item.t,
-          _id: item._id
+        cursor.forEach(function(item) {
+          if (!item) {
+            return;
+          }
+
+          features.push({
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [item.tln, item.tlt]
+            },
+            text: item.t,
+            _id: item._id
+          });
         });
       });
 
